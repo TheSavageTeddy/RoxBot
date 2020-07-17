9fcfdf2a06455b84778be78aa7e80c8c1f706fdd
Roxiun <ronan.naidu@hotmail.com>
2020-07-17 19:36:59 +0800

    Minecraft Server - changed region

--- utils/start_server.py
+++ utils/start_server.py
@@ -5,7 +5,7 @@ import requests
 
 def start_server():
 	minecraft_server = subprocess.Popen(["java", "-jar", "spigot-1.8.8-R0.1-SNAPSHOT-latest.jar"], cwd="/home/ronan/Server/BedWarsOnly") 
-	ngrok = subprocess.Popen(["./ngrok", "tcp", "25565"], cwd="/home/ronan/")
+	ngrok = subprocess.Popen(["./ngrok", "tcp", "-region", "au", "25565"], cwd="/home/ronan/")
 
 def get_ip():
 	#curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'
