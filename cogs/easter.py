4f93986fd7d21c859dd27341ff39b36ae8b872c2
TheSavageTeddy <51810476+TheSavageTeddy@users.noreply.github.com>
2020-07-23 15:12:47 +0800

    added Star Wars Easter egg
    I dont like sand

--- cogs/easter.py
+++ cogs/easter.py
@@ -15,5 +15,11 @@ class EasterEggs(commands.Cog):
         if "the earth king has invited you to lake laogai" in message.content.lower():
             await message.channel.send("I am honoured to accept his invitation.")
 
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "i dont like sand" in message.content.lower() or "i don't like sand" in message.content.lower() or "i do not like sand" in message.content.lower():
+            await message.channel.send("It's coarse, and rough, and irritating, and it gets everywhere.")
+
 def setup(bot):
     bot.add_cog(EasterEggs(bot))
