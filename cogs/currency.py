54ab02c791735da5efe8cda9893272c8bf757839
Roxiun <ronan.naidu@hotmail.com>
2020-07-24 16:40:40 +0800

    create currency - fix spelling

--- cogs/currency.py
+++ cogs/currency.py
@@ -6,11 +6,11 @@ from discord.ext import commands
 from datetime import datetime
 
 
-class Curency(commands.Cog):  
+class Currency(commands.Cog):  
     def __init__(self, bot):
         self.bot = bot
         self.config = getJSON("config.json")
     
 
 def setup(bot):
-    bot.add_cog(Curency(bot))
+    bot.add_cog(Currency(bot))
