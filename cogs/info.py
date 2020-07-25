19fa5ae1ff6372d787134a90a3d18e6d6ccf30fe
Roxiun <ronan.naidu@hotmail.com>
2020-07-25 19:08:03 +0800

    hyperlink

--- cogs/info.py
+++ cogs/info.py
@@ -102,8 +102,8 @@ class Info(commands.Cog):
             title='Invite me to your Server!',
             color=0x2ECC71
         )
-        invite_embed.add_field(name="Full Permissions", value=f"{self.config.bot_invite}")
-        invite_embed.add_field(name="Basic Permissions", value=f"{self.config.bot_invite_basic}")
+        invite_embed.add_field(name=f"[Full Permissions]({self.config.bot_invite})")
+        invite_embed.add_field(name=f"[Basic Permissions]({self.config.bot_invite_basic}})")
         invite_embed.set_footer(text="Made with ❤️ by Roxiun")
         await ctx.send(embed=invite_embed)
     
