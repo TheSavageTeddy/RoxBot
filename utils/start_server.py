6d507a37d49dd6e61111820e3de175e154a026cc
Roxiun <ronan.naidu@hotmail.com>
2020-07-25 20:04:55 +0800

    bedwars fix

--- utils/start_server.py
+++ utils/start_server.py
@@ -4,7 +4,7 @@ import requests
 import time
 
 def start_server():
-	minecraft_server = subprocess.Popen(["java", "-Xmx3G", "-jar", "spigot-1.8.8.jar"], cwd="/home/ronan/Server/BedWarsObly") #java -Dfile.encoding=UTF-8 -Xmx3G -jar 
+	minecraft_server = subprocess.Popen(["java", "-Xmx3G", "-jar", "spigot-1.8.8.jar"], cwd="/home/ronan/Server/BedWarsOnly") #java -Dfile.encoding=UTF-8 -Xmx3G -jar 
 	ngrok = subprocess.Popen(["./ngrok", "tcp", "-region", "au", "25565"], cwd="/home/ronan/")
 	return {"Minecraft":minecraft_server.pid, "ngrok":ngrok.pid}
 
