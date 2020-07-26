a9b408cbf297dc86859d1a1f91cdf44d7c4ce230
Roxiun <ronan.naidu@hotmail.com>
2020-07-26 15:11:24 +0800

    mute test

--- cogs/mod.py
+++ cogs/mod.py
@@ -170,6 +170,7 @@ class Moderator(commands.Cog):
             e.set_footer(text="Make Sure I have permissions to kick, and am higher than the specified member")
             await ctx.send(embed=e)
     
+    '''
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, discord.Forbidden):
@@ -179,7 +180,7 @@ class Moderator(commands.Cog):
             e = discord.Embed(description=":no_entry_sign: Something went wrong", colour=0xE74C3C)
             e.set_footer(text="Make Sure I have permissions to ban, and am higher than the specified member")
             await ctx.send(embed=e)
-
+    '''
 
 def setup(bot):
     bot.add_cog(Moderator(bot))
