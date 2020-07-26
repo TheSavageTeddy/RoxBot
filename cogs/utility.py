2e62bc4d38227ed789c2cb4e76912cdef0dcbdf3
Roxiun <ronan.naidu@hotmail.com>
2020-07-26 14:42:38 +0800

    dice update

--- cogs/utility.py
+++ cogs/utility.py
@@ -878,14 +878,14 @@ class Utility(commands.Cog):
 ]
 
         result=random.choice(outcomes)
-        e = discord.Embed(description=f"{result}\n\n```", colour=0x2ECC71)
+        e = discord.Embed(description=f"{result}```", colour=0x2ECC71)
         e.set_footer(text="Made with ❤️ by Roxiun & TheSavageTeddy!")
         msg = await ctx.send(embed=e)
 
         for i in range(7):
             await asyncio.sleep(0.1)
             result=random.choice(outcomes)
-            e = discord.Embed(description=f"{result}\n\n```", colour=0x2ECC71)
+            e = discord.Embed(description=f"{result}```", colour=0x2ECC71)
             e.set_footer(text="Made with ❤️ by Roxiun & TheSavageTeddy!")
             await msg.edit(
                 embed=e,
@@ -895,7 +895,7 @@ class Utility(commands.Cog):
         
         result=random.choice(outcomes)
         n = outcomes.index(result)
-        e = discord.Embed(description=f"{result}\nYou rolled a {n}!\n```", colour=0x2ECC71)
+        e = discord.Embed(description=f"{result}You rolled a {n+1}!```", colour=0x2ECC71)
         e.set_footer(text="Made with ❤️ by Roxiun & TheSavageTeddy!")
 
         await msg.edit(
